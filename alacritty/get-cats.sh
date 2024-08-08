ad2cb4ff97fd50cac337185d2f71180ff5dd62c0
Torstein Winjum <twinjum@hotmail.com>
2024-08-08 10:47:19 +0200

    add i3 config etc

---
+++ alacritty/get-cats.sh
@@ -0,0 +1,3 @@
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+
