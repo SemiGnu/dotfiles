b054888d6d8135d02b007b23f85bbcef5a44b0f7
Torstein Winjum <twinjum@hotmail.com>
2024-08-25 18:20:31 +0200

    fix nvim-tree setup

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -1,27 +1,30 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
     return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-    return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
+--[[ local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config") ]]
+--[[ if not config_status_ok then ]]
+--[[     print('lol no nvim-tree.config') ]]
+--[[     return ]]
+--[[ end ]]
+--[[]]
+--[[ local tree_cb = nvim_tree_config.nvim_tree_callback ]]
 
 nvim_tree.setup {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
+    --[[ open_on_setup = false, ]]
+    --[[ ignore_ft_on_setup = { ]]
+    --[[     "startify", ]]
+    --[[     "dashboard", ]]
+    --[[     "alpha", ]]
+    --[[ }, ]]
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
@@ -50,25 +53,25 @@ nvim_tree.setup {
     },
     view = {
         width = 30,
-        height = 30,
-        hide_root_folder = false,
         side = "left",
-        auto_resize = true,
-        mappings = {
-            custom_only = false,
-            list = {
-            { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-            { key = "h", cb = tree_cb "close_node" },
-            { key = "v", cb = tree_cb "vsplit" },
-            },
-        },
+        --[[ hide_root_folder = false, ]]
+        --[[ height = 30, ]]
+        --[[ auto_resize = true, ]]
+        --[[ mappings = { ]]
+        --[[     custom_only = false, ]]
+        --[[     list = { ]]
+        --[[     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" }, ]]
+        --[[     { key = "h", cb = tree_cb "close_node" }, ]]
+        --[[     { key = "v", cb = tree_cb "vsplit" }, ]]
+        --[[     }, ]]
+        --[[ }, ]]
         number = false,
         relativenumber = false,
     },
-    actions = {
-        quit_on_open = true,
-        window_picker = { enable = true },
-    },
+    --[[ actions = { ]]
+    --[[     quit_on_open = true, ]]
+    --[[     window_picker = { enable = true }, ]]
+    --[[ }, ]]
     renderer = {
         highlight_git = true,
         root_folder_modifier = ":t",
