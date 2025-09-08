fb924fc64283cab024a8198431de78da07fe1e44
Torstein Winjum <twinjum@hotmail.com>
2025-09-08 14:36:09 +0200

    add tmux, btop

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use 'nvim-lua/popup.nvim' -- An implementation of the Popup API from vim in Neovim
   use 'nvim-lua/plenary.nvim' -- Useful lua functions used ny lots of plugins
   use { 'catppuccin/nvim', as = 'catppuccin' }
+  use { 'Th3Whit3Wolf/one-nvim', as = 'atom-one' }
   use { 'ellisonleao/gruvbox.nvim', as = 'gruvbox' }
 
   -- cmp plugins
