274e7c65473eee4ce2fb928185376f097fe6ced0
Torstein Winjum <twinjum@hotmail.com>
2026-04-15 13:15:54 +0200

    add noice

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -17,6 +17,8 @@ vim.pack.add({
   'https://github.com/numToStr/Comment.nvim',
   'https://github.com/JoosepAlviste/nvim-ts-context-commentstring',
   'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/folke/noice.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
 
   'https://github.com/mason-org/mason.nvim',
 
